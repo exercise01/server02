81b9eb63063949e13330277e4f1575a1a92d1099
k.osawa <saataysu4408999@gmail.com>
2024-12-08 17:51:01 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+>npm run server
+
+http://localhost:9000/api/courses
+
+プリテイープリントに　チェックすると、JSONデータが読みやすくなる。
---
+++ db-data.ts
@@ -0,0 +1,40 @@
+export const COURSES:any ={
+1:{
+    id:1,
+    description:"angular-core",
+    iconUrl: '',
+    longDescription:"The most important part for angular-core", 
+    category: 'BEGINER'
+},
+2:{
+    id:2,
+    description:"angular-form",
+    iconUrl: '',
+    longDescription:"The most important part for angular-form", 
+    category: 'ADVANCE'
+}
+
+}
+
+export const USERS = {
+    1: {
+      id: 1,
+      email: 'test@angular-university.io',
+      password: 'test',
+      //pictureUrl: 'https://lh3.googleusercontent.com/-1pUNnTB3vaA/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdn4uEc0ti8YE4Uuw6_Kz04tVe2Mg.CMID/s32-c/photo.jpg'
+    }
+  
+  };
+
+  export function authenticate(email: string, password: string) {
+
+    const user: any = Object.values(USERS).find(user => user.email === email);
+  
+    if (user && user.password == password) {
+      return user;
+    } else {
+      return undefined;
+    }
+  
+  }
+  
